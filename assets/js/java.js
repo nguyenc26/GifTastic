d6fd32bbf8cf5f8f0c45fdff4e4a65633f56ff99
Cuong Desktop <nguyenc72694@gmail.com>
2019-07-31 21:36:34 -0500

    added assets and then started adding pseudocode to js file

---
+++ assets/js/java.js
@@ -0,0 +1,11 @@
+//adding an item event 
+//display gifs that have been grabbed 
+//animating gifs
+
+//array of things to scan through 
+var superheroes = ["Batman", "Superman", "Spider-man", "Green Lantern", "Hulk", "Wonder Woman", "Black Widow", "The Flash", "Wolverine", "Thor", "Deadpool", "Doctor Strange"]
+
+//scan through and creates a button for each item in the array
+function generatebuttons () {
+    
+}
