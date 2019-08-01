444ceffa936d286a3c5a9f0440e8be355901e164
Cuong Desktop <nguyenc72694@gmail.com>
2019-07-31 22:34:05 -0500

    added buttonmaker part

--- assets/js/java.js
+++ assets/js/java.js
@@ -6,6 +6,22 @@
 var superheroes = ["Batman", "Superman", "Spider-man", "Green Lantern", "Hulk", "Wonder Woman", "Black Widow", "The Flash", "Wolverine", "Thor", "Deadpool", "Doctor Strange"]
 
 //scan through and creates a button for each item in the array
-function generatebuttons () {
-    
+function generatebuttons() {
+    $("#buttons").empty();
+    for (var i = 0; i < superheroes.length; i++) {
+        var buttonmake = $("<button>")
+        buttonmake.addClass("heroName");
+        buttonmake.attr("data-name", superheroes[i]);
+        buttonmake.text(superheroes[i]);
+        $("#buttons").append(buttonmake);
+    } 
 }
+
+//addshow event
+
+//displays gif with api function 
+
+//click event listener for animating gifs
+
+
+generatebuttons()
