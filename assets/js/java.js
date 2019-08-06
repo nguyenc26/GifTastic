0efd4f165b6fa162339e0919a9c9a9ef83f8e125
Cuong Desktop <nguyenc72694@gmail.com>
2019-08-05 22:19:26 -0500

    got most things to work but still needs to work on adding a button and making the gifs still

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,6 +1,3 @@
-//adding an item event 
-//display gifs that have been grabbed 
-//animating gifs
 
 //array of things to scan through 
 var superheroes = ["Batman", "Superman", "Spider-man", "Green Lantern", "Hulk", "Wonder Woman", "Black Widow", "The Flash", "Wolverine", "Thor", "Dead pool", "Doctor Strange"]
@@ -16,7 +13,7 @@ function generatebuttons() {
         $("#buttons").append(buttonmake);
     }
 }
-
+//display gifs that have been grabbed 
 function displayGifs() {
     var superheroName = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + superheroName + "&limit=9&api_key=cN7u7lpi3S5MZebnA1Mgl9XgW1ConvL5";
@@ -26,14 +23,51 @@ function displayGifs() {
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        var results = response.data;
+
+        for ( var i = 0; i < results.length; i++){
+            var gifDiv = $("<div class=gifs>")
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var heroImage = $("<img>");
+            heroImage.attr("src", results[i].images.fixed_height.url);
+            heroImage.attr('data-still', results[i].images.fixed_height_still.url);
+			heroImage.attr('data-state', 'still');
+			heroImage.attr('data-animate', results[i].images.fixed_height.url);
+            
+            gifDiv.append(p);
+            gifDiv.append(heroImage);
+            $("#gifarea").prepend(gifDiv);
+            
+            
+        }
     });
 
 }
-//addshow event
+//adding an item event 
+$("#addhero").on("click", function(){
+	var newHero = $("#superheroes-input").val().trim();
+	superheroes.push(newHero);
+	makeButtons();
+})
 
-//displays gif with api function 
+//displays gif with api function
 
 //click event listener for animating gifs
+$(".gifs").on("click", function () {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
+
+
+
 $(document).on("click", ".heroName", displayGifs);
 
 generatebuttons()
