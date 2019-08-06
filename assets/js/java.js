8796e6b9bfc800862dec9e0d96d9cad960de86d3
Cuong Desktop <nguyenc72694@gmail.com>
2019-08-05 21:35:09 -0500

    got the gifs to get from ajax.

--- assets/js/java.js
+++ assets/js/java.js
@@ -3,7 +3,7 @@
 //animating gifs
 
 //array of things to scan through 
-var superheroes = ["Batman", "Superman", "Spider-man", "Green Lantern", "Hulk", "Wonder Woman", "Black Widow", "The Flash", "Wolverine", "Thor", "Deadpool", "Doctor Strange"]
+var superheroes = ["Batman", "Superman", "Spider-man", "Green Lantern", "Hulk", "Wonder Woman", "Black Widow", "The Flash", "Wolverine", "Thor", "Dead pool", "Doctor Strange"]
 
 //scan through and creates a button for each item in the array
 function generatebuttons() {
@@ -14,14 +14,26 @@ function generatebuttons() {
         buttonmake.attr("data-name", superheroes[i]);
         buttonmake.text(superheroes[i]);
         $("#buttons").append(buttonmake);
-    } 
+    }
 }
 
+function displayGifs() {
+    var superheroName = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + superheroName + "&limit=9&api_key=cN7u7lpi3S5MZebnA1Mgl9XgW1ConvL5";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+
+}
 //addshow event
 
 //displays gif with api function 
 
 //click event listener for animating gifs
-
+$(document).on("click", ".heroName", displayGifs);
 
 generatebuttons()
